df94921bd53ae926f6cd175903e5fb8c913a2d14
ajexks284 <76217648+ajexks284@users.noreply.github.com>
2021-03-19 15:19:36 +0200

    Add sort list function

--- src/app.js
+++ src/app.js
@@ -2,16 +2,22 @@ import { UI } from './ui';
 import { events } from './pubsub';
 
 export const app = (function() {
-    const toDoList = [{title: 'Do homework', dueDate: '2021-03-17', project: 'default', priority: 4, isCompleted: false},
-                      {title: 'Go to school', dueDate: '2021-03-19', project: 'default', priority: 2, isCompleted: false},
-                      {title: 'Workout', dueDate: '2021-03-18', project: 'default', priority: 3, isCompleted: false},
-                      {title: 'Sleep', dueDate: '2021-03-22', project: 'default', priority: 1, isCompleted: false},
-                      {title: 'Sleep again', dueDate: '2021-04-03', project: 'default', priority: 4, isCompleted: false}];
+    const toDoList = [{title: 'Do homework', dueDate: '2021-03-17', project: 'School', priority: 4, isCompleted: false},
+                      {title: 'Go to school', dueDate: '2021-03-19', project: 'School', priority: 2, isCompleted: false},
+                      {title: 'Workout', dueDate: '2021-03-18', project: 'Workout', priority: 3, isCompleted: false},
+                      {title: 'Sleep', dueDate: '2021-03-22', project: 'Recreational', priority: 1, isCompleted: false},
+                      {title: 'Sleep again', dueDate: '2021-04-03', project: 'Recreational', priority: 4, isCompleted: false}]
+
+    function sortList() {
+        toDoList.sort((a, b) => {
+            return a.priority === b.priority ? 0 : (a.priority > b.priority ? 1 : -1);
+        });
+    }
 
     // To Do Factory
     const createToDo = (title, dueDate, project, priority) => {
         let isCompleted = false;
-        project = project || 'default';
+        project = project || 'Default';
         return {title, dueDate, project, priority, isCompleted};
     }
 
@@ -45,5 +51,5 @@ export const app = (function() {
     events.on('taskDeleted', deleteToDo);
     events.on('taskAdded', addToDo);
 
-    return { getCurrentToDoList }
+    return { getCurrentToDoList, sortList }
 })();
