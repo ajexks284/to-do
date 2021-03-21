b20f1f67d35865234201c6ed2025379c2db5dcbd
ajexks284 <76217648+ajexks284@users.noreply.github.com>
2021-03-21 17:57:38 +0200

    Add filters. Add function for fixing bug where items lose display none after rendering

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+import { app } from './app';
 import { UI } from './ui';
+import { projects } from './projects';
 import { projectsUI } from './projects';
 import { counter } from './counter';
 
@@ -13,4 +15,51 @@ document.querySelector('#task-due-date').min = `${year}-${month}-${day}`;
 
 UI.render();
 projectsUI.render();
-counter.incrementProjectsCounter();
+counter.incrementCounters();
+
+
+// Filters
+const homeSectionFilter = document.querySelector('.home-div-name');
+homeSectionFilter.addEventListener('click', () => {
+    let taskList = document.querySelectorAll('.to-do-item');
+    [...taskList].forEach(task => {
+        task.style.display = 'flex';
+    })
+})
+
+const todaySectionFilter = document.querySelector('.today-div-name');
+todaySectionFilter.addEventListener('click', () => {
+    let taskList = document.querySelectorAll('.to-do-item');
+    [...taskList].forEach(task => {
+        let todayDateDay = new Date().getDate();
+        if (todayDateDay < 10) todayDateDay = `0${todayDateDay}`;
+        let taskDateDay = task.firstElementChild.nextElementSibling.firstElementChild.innerText.slice(-2);
+
+        if (taskDateDay == todayDateDay) {
+            task.style.display = 'flex';
+        } else {
+            task.style.display = 'none';
+        }
+    })
+})
+
+const thisWeekSectionFilter = document.querySelector('.this-week-div-name');
+thisWeekSectionFilter.addEventListener('click', () => {
+    // 
+})
+
+// Helps keep display none on tasks after rendering
+export function returnItemsWithDisplayNone() {
+    let taskList = document.querySelectorAll('.to-do-item');
+    const displayNoneItems = {};
+
+    taskList.forEach(item => {
+        if (item.style.display == 'none') {
+            displayNoneItems[item.firstElementChild.lastElementChild.innerText] = 'none';
+        } else {
+            displayNoneItems[item.firstElementChild.lastElementChild.innerText] = 'flex';
+        }
+    })
+    
+    return displayNoneItems;
+}
